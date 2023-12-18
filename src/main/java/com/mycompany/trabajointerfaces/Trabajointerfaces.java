2718be804ad2f5be5a889e9f8ff9dd601129b2ed
Peter13 <pedro.garvic@educa.jcyl.es>
2023-12-18 17:33:20 +0100

    commit con pantallas

---
+++ src/main/java/com/mycompany/trabajointerfaces/Trabajointerfaces.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajointerfaces;
+
+/**
+ *
+ * @author Bypet
+ */
+public class Trabajointerfaces {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
